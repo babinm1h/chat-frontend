261f3a81909d9c035afc2a1bd07e237d9c0aef4f
@babinm1h <babinmih@mail.ru>
2023-01-08 19:33:27 +0300

    dpl

--- src/context/socketContext.ts
+++ src/context/socketContext.ts
@@ -1,9 +1,6 @@
 import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
-export const socket = io(process.env.REACT_APP_API_URL || 'http://localhost:7777', {
-  withCredentials: true,
-  transports: ['websocket'],
-});
+export const socket = io(process.env.REACT_APP_API_URL || 'http://localhost:7777', { withCredentials: true });
 
 export const SocketContext = createContext(socket);
